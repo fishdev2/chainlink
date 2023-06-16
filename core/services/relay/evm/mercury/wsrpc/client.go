2d6b8cd1f8fe504331dbd8e40671adb89d50092b
Sam Davies <samsondav@protonmail.com>
2023-06-16 08:08:54 -0400

    Redial mercury server after 5 consecutive transmit failures
    If we get 5+ request timeouts in a row, close and re-open the websocket
    connection. This *shouldn't* be necessary in theory (wsrpc should handle
    it for us) but it acts as a "belts and braces" approach to ensure we get
    a websocket connection back up and running again if it gets itself into
    a bad state

--- core/services/relay/evm/mercury/wsrpc/client.go
+++ core/services/relay/evm/mercury/wsrpc/client.go
@@ -2,6 +2,8 @@ package wsrpc
 
 import (
 	"context"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -15,6 +17,8 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/mercury/wsrpc/pb"
 	"github.com/smartcontractkit/chainlink/v2/core/utils"
 )
+
+const MaxConsecutiveTransmitFailures = 5
 
 type Client interface {
 	services.ServiceCtx
@@ -31,6 +35,11 @@ type client struct {
 	logger logger.Logger
 	conn   *wsrpc.ClientConn
 	client pb.MercuryClient
+
+	consecutiveTimeoutCnt atomic.Int32
+	wg                    sync.WaitGroup
+	chStop                utils.StopChan
+	chResetTransport      chan struct{}
 }
 
 // Consumers of wsrpc package should not usually call NewClient directly, but instead use the Pool
@@ -40,39 +49,90 @@ func NewClient(lggr logger.Logger, clientPrivKey csakey.KeyV2, serverPubKey []by
 
 func newClient(lggr logger.Logger, clientPrivKey csakey.KeyV2, serverPubKey []byte, serverURL string) *client {
 	return &client{
-		csaKey:       clientPrivKey,
-		serverPubKey: serverPubKey,
-		serverURL:    serverURL,
-		logger:       lggr.Named("WSRPC"),
+		csaKey:           clientPrivKey,
+		serverPubKey:     serverPubKey,
+		serverURL:        serverURL,
+		logger:           lggr.Named("WSRPC"),
+		chResetTransport: make(chan struct{}, 1),
 	}
 }
 
 func (w *client) Start(_ context.Context) error {
 	return w.StartOnce("WSRPC Client", func() error {
-		// NOTE: Dial is non-blocking, and will retry on an exponential backoff
-		// in the background until close is called, or context is cancelled.
-		// This is why we use the background context, not the start context here.
-		//
-		// Any transmits made while client is still trying to dial will fail
-		// with error.
-		conn, err := wsrpc.DialWithContext(context.Background(), w.serverURL,
-			wsrpc.WithTransportCreds(w.csaKey.Raw().Bytes(), w.serverPubKey),
-			wsrpc.WithLogger(w.logger),
-		)
-		if err != nil {
-			setLivenessMetric(false)
-			return errors.Wrap(err, "failed to dial wsrpc client")
+		if err := w.dial(context.Background()); err != nil {
+			return err
 		}
-		setLivenessMetric(true)
-		w.conn = conn
-		w.client = pb.NewMercuryClient(conn)
+		w.wg.Add(1)
+		go w.runloop()
 		return nil
 	})
 }
 
+// NOTE: Dial is non-blocking, and will retry on an exponential backoff
+// in the background until close is called, or context is cancelled.
+// This is why we use the background context, not the start context here.
+//
+// Any transmits made while client is still trying to dial will fail
+// with error.
+func (w *client) dial(ctx context.Context, opts ...wsrpc.DialOption) error {
+	conn, err := wsrpc.DialWithContext(ctx, w.serverURL,
+		append(opts,
+			wsrpc.WithTransportCreds(w.csaKey.Raw().Bytes(), w.serverPubKey),
+			wsrpc.WithLogger(w.logger),
+		)...,
+	)
+	if err != nil {
+		setLivenessMetric(false)
+		return errors.Wrap(err, "failed to dial wsrpc client")
+	}
+	setLivenessMetric(true)
+	w.conn = conn
+	w.client = pb.NewMercuryClient(conn)
+	return nil
+}
+
+func (w *client) runloop() {
+	defer w.wg.Done()
+	for {
+		select {
+		case <-w.chStop:
+			return
+		case <-w.chResetTransport:
+			// Using channel here ensures we only have one reset in process at
+			// any given time
+			w.resetTransport()
+		}
+	}
+}
+
+// resetTransport disconnects and reconnects to the mercury server
+func (w *client) resetTransport() {
+	ok := w.IfStarted(func() {
+		w.conn.Close() // Close is safe to call multiple times
+	})
+	if !ok {
+		panic("resetTransport should never be called unless client is in 'started' state")
+	}
+	ctx, cancel := w.chStop.Ctx(context.Background())
+	defer cancel()
+	b := utils.NewRedialBackoff()
+	for {
+		// Will block until successful dial, or context is canceled (i.e. on close)
+		if err := w.dial(ctx, wsrpc.WithBlock()); err != nil {
+			w.logger.Errorw("ResetTransport failed to redial", "err", err)
+			time.Sleep(b.Duration())
+		} else {
+			w.logger.Info("ResetTransport successfully redialled")
+			break
+		}
+	}
+}
+
 func (w *client) Close() error {
 	return w.StopOnce("WSRPC Client", func() error {
+		close(w.chStop)
 		w.conn.Close()
+		w.wg.Wait()
 		return nil
 	})
 }
@@ -97,23 +157,48 @@ func (w *client) Healthy() (err error) {
 	return nil
 }
 
-func (w *client) Transmit(ctx context.Context, req *pb.TransmitRequest) (resp *pb.TransmitResponse, err error) {
-	lggr := w.logger.With("req.Payload", hexutil.Encode(req.Payload))
-	lggr.Debug("Transmit")
-	start := time.Now()
+func (w *client) waitForReady(ctx context.Context) (err error) {
 	ok := w.IfStarted(func() {
 		if ready := w.conn.WaitForReady(ctx); !ready {
 			err = errors.Errorf("websocket client not ready; got state: %v", w.conn.GetState())
 			return
 		}
-
-		resp, err = w.client.Transmit(ctx, req)
 	})
 	if !ok {
-		return nil, errors.New("client is not started")
+		return errors.New("client is not started")
+	}
+	return
+}
+
+func (w *client) Transmit(ctx context.Context, req *pb.TransmitRequest) (resp *pb.TransmitResponse, err error) {
+	lggr := w.logger.With("req.Payload", hexutil.Encode(req.Payload))
+	lggr.Debug("Transmit")
+	start := time.Now()
+	if err := w.waitForReady(ctx); err != nil {
+		return nil, errors.Wrap(err, "Transmit failed")
+	}
+	resp, err = w.client.Transmit(ctx, req)
+	if errors.Is(err, context.DeadlineExceeded) {
+		cnt := w.consecutiveTimeoutCnt.Add(1)
+		if cnt == MaxConsecutiveTransmitFailures {
+			w.logger.Errorf("Timed out on %d consecutive transmits, resetting transport", cnt)
+			// HACK: if we get 5+ request timeouts in a row, close
+			// and re-open the websocket connection. This *shouldn't* be
+			// necessary in theory (wsrpc should handle it for us) but it acts
+			// as a "belts and braces" approach to ensure we get a websocket
+			// connection back up and running again if it gets itself into a
+			// bad state
+			select {
+			case w.chResetTransport <- struct{}{}:
+			default:
+				// TODO: how to handle the case where a reset is already happening? How can this even happen?
+			}
+		}
+	} else {
+		w.consecutiveTimeoutCnt.Store(0)
 	}
 	if err != nil {
-		lggr.Errorw("Transmit failed", "err", err, "req", req, "resp", resp)
+		lggr.Warnw("Transmit failed", "err", err, "req", req, "resp", resp)
 		incRequestStatusMetric(statusFailed)
 	} else {
 		lggr.Debugw("Transmit succeeded", "resp", resp)
@@ -126,17 +211,10 @@ func (w *client) Transmit(ctx context.Context, req *pb.TransmitRequest) (resp *p
 func (w *client) LatestReport(ctx context.Context, req *pb.LatestReportRequest) (resp *pb.LatestReportResponse, err error) {
 	lggr := w.logger.With("req.FeedId", hexutil.Encode(req.FeedId))
 	lggr.Debug("LatestReport")
-	ok := w.IfStarted(func() {
-		if ready := w.conn.WaitForReady(ctx); !ready {
-			err = errors.Errorf("websocket client not ready; got state: %v", w.conn.GetState())
-			return
-		}
-
-		resp, err = w.client.LatestReport(ctx, req)
-	})
-	if !ok {
-		return nil, errors.Errorf("client is not started; state=%v", w.StartStopOnce.State())
+	if err := w.waitForReady(ctx); err != nil {
+		return nil, errors.Wrap(err, "LatestReport failed")
 	}
+	resp, err = w.client.LatestReport(ctx, req)
 	if err != nil {
 		lggr.Errorw("LatestReport failed", "err", err, "req", req, "resp", resp)
 	} else if resp.Error != "" {
