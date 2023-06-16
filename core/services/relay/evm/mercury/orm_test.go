e8cae27495bc556a6edd2b1fe5f6819a59489a4a
Martin Liu <martin.liu@smartcontract.com>
2023-06-17 06:37:05 +1000

    wip

---
+++ core/services/relay/evm/mercury/orm_test.go
@@ -0,0 +1,73 @@
+package mercury
+
+import (
+	"testing"
+
+	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils/pgtest"
+	"github.com/smartcontractkit/chainlink/v2/core/logger"
+	"github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/mercury/wsrpc/pb"
+)
+
+func TestORM(t *testing.T) {
+	db := pgtest.NewSqlxDB(t)
+	lggr := logger.TestLogger(t)
+	orm := NewORM(db, lggr, pgtest.NewQConfig(true))
+
+	reportContext := ocrtypes.ReportContext{
+		ReportTimestamp: ocrtypes.ReportTimestamp{
+			ConfigDigest: ocrtypes.ConfigDigest{'1'},
+			Epoch:        10,
+			Round:        20,
+		},
+		ExtraHash: [32]byte{'2'},
+	}
+
+	err := orm.InsertTransmitRequest(&pb.TransmitRequest{Payload: []byte("report-1")}, reportContext)
+	require.NoError(t, err)
+	err = orm.InsertTransmitRequest(&pb.TransmitRequest{Payload: []byte("report-2")}, reportContext)
+	require.NoError(t, err)
+	err = orm.InsertTransmitRequest(&pb.TransmitRequest{Payload: []byte("report-3")}, reportContext)
+	require.NoError(t, err)
+
+	transmissions, err := orm.GetTransmitRequests()
+	require.NoError(t, err)
+	require.Equal(t, transmissions, []*Transmission{
+		{Req: &pb.TransmitRequest{Payload: []byte("report-1")}, ReportCtx: reportContext},
+		{Req: &pb.TransmitRequest{Payload: []byte("report-2")}, ReportCtx: reportContext},
+		{Req: &pb.TransmitRequest{Payload: []byte("report-3")}, ReportCtx: reportContext},
+	})
+
+	err = orm.DeleteTransmitRequest(&pb.TransmitRequest{Payload: []byte("report-2")})
+	require.NoError(t, err)
+
+	transmissions, err = orm.GetTransmitRequests()
+	require.NoError(t, err)
+	require.Equal(t, transmissions, []*Transmission{
+		{Req: &pb.TransmitRequest{Payload: []byte("report-1")}, ReportCtx: reportContext},
+		{Req: &pb.TransmitRequest{Payload: []byte("report-3")}, ReportCtx: reportContext},
+	})
+
+	err = orm.DeleteTransmitRequest(&pb.TransmitRequest{Payload: []byte("does-not-exist")})
+	require.NoError(t, err)
+
+	transmissions, err = orm.GetTransmitRequests()
+	require.NoError(t, err)
+	require.Equal(t, transmissions, []*Transmission{
+		{Req: &pb.TransmitRequest{Payload: []byte("report-1")}, ReportCtx: reportContext},
+		{Req: &pb.TransmitRequest{Payload: []byte("report-3")}, ReportCtx: reportContext},
+	})
+
+	err = orm.InsertTransmitRequest(&pb.TransmitRequest{Payload: []byte("report-4")}, reportContext)
+	require.NoError(t, err)
+
+	transmissions, err = orm.GetTransmitRequests()
+	require.NoError(t, err)
+	require.Equal(t, transmissions, []*Transmission{
+		{Req: &pb.TransmitRequest{Payload: []byte("report-1")}, ReportCtx: reportContext},
+		{Req: &pb.TransmitRequest{Payload: []byte("report-3")}, ReportCtx: reportContext},
+		{Req: &pb.TransmitRequest{Payload: []byte("report-4")}, ReportCtx: reportContext},
+	})
+}
